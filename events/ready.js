b5634d8554876a46f5c669c708d177996737cb1e
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-06-22 13:40:04 +0300

    ayee1

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,8 @@
+const Discord = require("discord.js");
+
 module.exports = async (bot) => {
+        bot.commands = new Discord.Collection();
+
         console.log('online motherfucker');
         console.log(bot.commands);
         bot.user.setActivity('UnDeadCraftOfficial | prefix "/"');
