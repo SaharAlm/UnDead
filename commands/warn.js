f6aeb2232699cb5ba2d83e48df5ae0ffa2ee3f84
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-07-05 19:10:53 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = require('./warnings.json')
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
