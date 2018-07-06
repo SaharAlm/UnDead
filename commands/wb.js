11b8633f8dd5fe3fb0aa46b29e893dd46579a88b
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-07-06 13:04:56 +0300

    Update wb.js

--- commands/wb.js
+++ commands/wb.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("**Our website** - https://bit.ly/2KHDuez")
+    message.channel.send("**Our website** - https://bit.ly/2KHDuez");
 }   
 
 module.exports.conf = {
-    name: "wb",
+    name: "web",
     description: "The Website!"
 }
