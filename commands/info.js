99b237b82ecbc40391ca58f3736c398fadd2b776
Twins1 <33881658+Twins1@users.noreply.github.com>
2018-07-06 13:24:56 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args, err) => {
     .setAuthor(message.author.tag, message.author.avatrURL)
     .setColor("RANDOM")
     .addField("/ip", 'This is ip command!')
-    .addField("/wb", 'To get the website link!')
+    .addField("/web", 'To get the website link!')
     .addField("/bank", 'To see how much coins you have in the bank')
     .addField("/user", 'This is the user info');
      message.channel.send(embed);
